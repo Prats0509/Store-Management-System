6451bdfbdfe5b6d3fbf15cc2ee1e0c48e8b1f5e9
pratiksha <pratiksha.dutta@lcieducation.net>
2023-12-01 00:42:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,380 @@
+﻿namespace Store_Dutta_Kaur
+{
+    #region Structs -@Pratiksha
+
+    // Struct for Employee -@Pratiksha.
+    struct Employee
+    {
+        public int UniqueId;
+        public string Password;
+        //primary constructor for structure Employee -@Pratiksha
+        public Employee(int uniqueId, string password)
+        {
+            UniqueId = uniqueId;
+            Password = password;
+        }
+    }
+
+    // Struct for Client  -@Pratiksha 
+    struct Client
+    {
+        public int UniqueId;
+        public string FirstName;
+        public string LastName;
+        public string Password;
+        //primary constructor for structure Client -@Pratiksha
+        public Client(int uniqueId, string firstName, string lastName, string password)
+        {
+            UniqueId = uniqueId;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = password;
+        }
+    }
+
+    // Struct for Product -@Pratiksha
+    struct Product
+    {
+        public int UniqueId;
+        public string Name;
+        public double UnitPrice;
+        public int QuantityAvailable;
+        //primary constructor for structure Product -@Pratiksha
+        public Product(int uniqueId, string name, double unitPrice, int quantityAvailable)
+        {
+            UniqueId = uniqueId;
+            Name = name;
+            UnitPrice = unitPrice;
+            QuantityAvailable = quantityAvailable;
+        }
+    }
+
+    // Struct for Purchase -@Pratiksha
+    struct Purchase
+    {
+        public Client Client;
+        public Product[] Products;
+        public double Subtotal;
+        public double Taxes;
+        public double TotalPrice;
+        //primary constructor for structure Product -@Pratiksha
+        public Purchase(Client client, Product[] products, double subtotal, double taxes, double totalPrice)
+        {
+            Client = client;
+            Products = products;
+            Subtotal = subtotal;
+            Taxes = taxes;
+            TotalPrice = totalPrice;
+        }
+    }
+
+    #endregion
+    internal class Program
+    {
+        #region Array Storage and constants -@Pratiksha
+        // Array to store employees -@Pratiksha
+        static Employee[] employee = new Employee[1];
+        // Array to hold employees -@Pratiksha
+        static Client[] client = new Client[1];
+        // Array to hold products -@Pratiksha
+        static Product[] product = new Product[100];
+        // Array to hold sales -@Pratiksha
+        static Purchase[] sale = new Purchase[100];
+
+        // Constants for default data -@Pratiksha
+        const int defaultEmployeeId = 111111;
+        const string defaultEmployeePassword = "tester";
+        const string defaultClientPassword = "clientPass";
+
+        #endregion 
+
+        #region Input Validations -@Pratiksha 
+        //Function to read an Integer , if not retry with error message
+        static int readInteger()
+        {
+            int toReturn = 0;
+            while (!Int32.TryParse(Console.ReadLine(), out toReturn))
+            {
+                Console.Write("Invalid integer value, please try again: ");
+            }
+
+            return toReturn;
+        }
+
+        //Function to ReadInteger within a range
+        static int readInteger(int min, int max)
+        {
+            int toReturn = readInteger();
+            while (!isWithinRange(toReturn, min, max))
+            {
+                Console.Write($"The value must be between {min} and {max}. Please try again: ");
+                toReturn = readInteger();
+            }
+
+            return toReturn;
+        }
+
+        //Function to check if in range
+        static bool isWithinRange(int num, int min, int max)
+        {
+            return num >= min && num <= max;
+        }
+
+        //Function to read a double
+        static double readDouble()
+        {
+            double toReturn = 0;
+            while (!Double.TryParse(Console.ReadLine(), out toReturn))
+            {
+                Console.Write("Invalid real value, please try again: ");
+            }
+
+            return toReturn;
+        }
+
+        //Function to read a double in a range
+        static double readDouble(double min, double max)
+        {
+            double toReturn = readDouble();
+            while (!isWithinRange(toReturn, min, max))
+            {
+                Console.Write($"The value must be between {min} and {max}. Please try again: ");
+                toReturn = readDouble();
+            }
+
+            return toReturn;
+        }
+
+        //Function to check if double is within the range
+        static bool isWithinRange(double num, double min, double max)
+        {
+            return num >= min && num <= max;
+        }
+
+        //Function to read a string or retry is empty or null
+        static string readString()
+        {
+            string toReturn = null;
+            while (String.IsNullOrEmpty(toReturn))
+            {
+                toReturn = Console.ReadLine();
+            }
+
+            return toReturn;
+        }
+
+        #endregion
+
+        #region Initialize default data -@Pratiksha
+        static void initializeDefaultData()
+        {
+            // Initialize default employee -@Pratiksha
+            employee[0] = new Employee(defaultEmployeeId, defaultEmployeePasswor d);
+
+            // Initialize default products -@Pratiksha
+            product[0] = new Product(100001, "Product 1", 10.99, 50);
+            product[1] = new Product(100002, "Product 2", 5.75, 30);
+            product[2] = new Product(100003, "Product 3", 7.49, 25);
+            product[3] = new Product(100004, "Product 4", 15.25, 40);
+            product[4] = new Product(100005, "Product 5", 3.99, 20);
+            product[5] = new Product(100006, "Product 6", 8.50, 60);
+            product[6] = new Product(100007, "Product 7", 12.75, 35);
+            product[7] = new Product(100008, "Product 8", 6.80, 45);
+            product[8] = new Product(100009, "Product 9", 4.25, 55);
+            product[9] = new Product(100010, "Product 10", 9.99, 70);
+
+            // Initialize default client -@Pratiksha
+            client[0] = new Client(100001, "Default", "Client", defaultClientPassword);
+
+        }
+        #endregion
+
+        #region Menu Displays -@Pratiksha
+        //function to display the main menu lines -@Pratiksha
+        static void displayMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1.Sign In");
+            Console.WriteLine("2.Exit Application");
+            Console.WriteLine("Please enter the appropriate option");
+        }
+        //function to display the role menu lines -@Pratiksha
+        static void displayRoleMenu()
+        {          
+            Console.WriteLine("1.Employee");
+            Console.WriteLine("2.Client");
+            Console.WriteLine("Please enter the appropriate option");
+        }
+        //function to display the client menu lines -@Pratiksha
+        static void displayClientMenu()
+        {
+            Console.WriteLine("1.Display all products");
+            Console.WriteLine("2.Display purchases");
+            Console.WriteLine("3.Sign Out");
+            Console.WriteLine("Please enter the appropriate option (1-3)");
+        }
+        #endregion
+
+        #region Client Verification -@Pratiksha
+        //function to get the password from console and verify length -@Pratiksha
+        static string getPassword()
+        {
+            {
+                string toReturn = null;
+                while (String.IsNullOrEmpty(toReturn) || toReturn.Length < 6 || toReturn.Length > 10)
+                {
+                    toReturn = Console.ReadLine();
+                }
+
+                return toReturn;
+            }
+        }
+        /** function to check the unique ID and password and return
+        the client if found or return a client with ID 0  -@Pratiksha
+        **/
+        static Client verifyClient()
+        {
+            Console.WriteLine("Enter Client ID:");
+            //read an interger between 100000 and 999999 -@Pratiksha
+            int clientId = readInteger(100000, 999999);
+
+            //read a string which is not empty or null -@Pratiksha
+            Console.WriteLine("Enter Password:");
+            string password = getPassword();
+
+            //for loop to go throught the record and match -@Pratiksha
+            for (int i = 0; i < client.Length; i++)
+            {
+                if (client[i].UniqueId == clientId && client[i].Password == password)
+                {
+                    return client[i]; // Return verified client -@Pratiksha
+                }
+            }
+            // Return a Client with ID 0 if verify fails -@Pratiksha
+            return new Client { UniqueId = 0 };
+        }
+        #endregion
+
+        #region Client Functionality -@Pratiksha
+        //function to display all products -@Pratiksha
+        static void displayAllProducts()
+        {
+            Console.WriteLine("List of Products:");
+
+            for (int i = 0; i < product.Length; i++)
+            {
+                Product productInfo = product[i];
+                Console.WriteLine($"ID: {productInfo.UniqueId}, Name: {productInfo.Name}, Price: {productInfo.UnitPrice}, Quantity Available: {productInfo.QuantityAvailable}");
+            }
+        }
+
+        //function to display purchases -@Pratiksha
+        static void displayPurchases()
+        {
+            throw new NotImplementedException();
+        }
+
+        //function to display Client Menu and perform client functions -@Pratiksha
+        static void handleClientFunctionality(Client client)
+        {
+            bool exit = false;
+            
+            while (!exit)
+            {
+                
+                //function to client menu -@Pratiksha
+                displayClientMenu();
+                
+                //read the choice between 1 and 3 from the menu -@Pratiksha
+                int getChoice = readInteger(1, 3);
+
+                //switch statement for handling client functions -@Pratikha
+                switch (getChoice)
+                {
+                    case 1:
+                        displayAllProducts();
+                        break;
+                    case 2:
+                        displayPurchases();
+                        break;
+                    case 3:
+                        exit = true;
+                        Console.WriteLine("Signing out....");
+                        break;
+                }
+            }
+            if (exit)
+            {
+                // return to main menu after signing out -@Pratikha
+                startProgram();
+            }
+        }
+        #endregion 
+
+        #region Exit Application Functionality -@Pratiksha
+        //Function to safely exit the application with confirmation -@Pratiksha
+        static void exit()
+        {
+            Console.WriteLine("Are you sure you want to exit? (Type Yes)");
+            string input = readString().ToUpper();
+            if (input == "YES")
+            {
+                Console.WriteLine("Exiting the application...");
+                Environment.Exit(0);
+            }
+        }
+        #endregion
+
+        #region App Functionality -@Pratiksha 
+
+        //Core function to run the logic of the App -@Pratiksha (client) -@Gurleen (employee)
+        static void startProgram()
+        {
+            //display the main menu and take the inputs  -@Pratiksha
+            displayMainMenu();
+            int choiceMainMenu = readInteger(1, 2);
+            if (choiceMainMenu == 1)
+            {
+                //display the role menu and take the inputs  -@Pratiksha
+                displayRoleMenu();
+                int roleChoice = readInteger(1, 2);
+                if (roleChoice == 1)
+                {
+                    // display employee and functions -@Pratiksha
+                }
+                else if (roleChoice == 2)
+                {
+                    //verify the client by password -@Pratiksha
+                    Client client = verifyClient();
+                    if (client.UniqueId != 0)
+                    {
+                        //handle the client functioanlities with menu -@Pratiksha
+                        handleClientFunctionality(client);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to authenticate user");
+                        startProgram();
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Exiting the application");
+                exit();
+            }
+
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            initializeDefaultData();
+            startProgram();
+        }
+    }
+
+
+}
