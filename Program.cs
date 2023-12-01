eeedb26c4b58b474d5e6f882804bc1a729875de5
gurleenmatta <gurleen.m@somaiya.edu>
2023-12-01 03:02:20 -0500

    Add employee code

--- Program.cs
+++ Program.cs
@@ -71,13 +71,19 @@
     #endregion
     internal class Program
     {
-        #region Array Storage and constants -@Pratiksha
-        // Array to store employees -@Pratiksha
-        static Employee[] employee = new Employee[1];
-        // Array to hold employees -@Pratiksha
-        static Client[] client = new Client[1];
+        #region Array Storage and constants -@Pratiksha and -@Gurleen
+        // Constants and Variables for default data -@Gurleen
+        const int maxProducts = 100;
+        const int maxClients = 10;
+        static int productsCount = 0;
+        static int clientsCount = 0;
+
+        // Variable to store employee -@Gurleen
+        static Employee employee;
+        // Array to hold clients -@Pratiksha and -@Gurleen
+        static Client[] client = new Client[maxClients];
         // Array to hold products -@Pratiksha
-        static Product[] product = new Product[100];
+        static Product[] product = new Product[maxProducts];
         // Array to hold sales -@Pratiksha
         static Purchase[] sale = new Purchase[100];
 
@@ -85,6 +91,7 @@
         const int defaultEmployeeId = 111111;
         const string defaultEmployeePassword = "tester";
         const string defaultClientPassword = "clientPass";
+
 
         #endregion 
 
@@ -155,21 +162,23 @@
         static string readString()
         {
             string toReturn = null;
-            while (String.IsNullOrEmpty(toReturn))
+            do
             {
                 toReturn = Console.ReadLine();
-            }
+                if (toReturn == null)
+                    Console.Write("Invalid string value, please try again: ");
+            } while (String.IsNullOrEmpty(toReturn));
 
             return toReturn;
         }
 
         #endregion
 
-        #region Initialize default data -@Pratiksha
+        #region Initialize default data -@Pratiksha and -@Gurleen
         static void initializeDefaultData()
         {
-            // Initialize default employee -@Pratiksha
-            employee[0] = new Employee(defaultEmployeeId, defaultEmployeePasswor d);
+            // Initialize default employee -@Gurleen
+            employee = new Employee(defaultEmployeeId, defaultEmployeePassword);
 
             // Initialize default products -@Pratiksha
             product[0] = new Product(100001, "Product 1", 10.99, 50);
@@ -186,10 +195,16 @@
             // Initialize default client -@Pratiksha
             client[0] = new Client(100001, "Default", "Client", defaultClientPassword);
 
+            // Initialize default products count -@Gurleen
+            productsCount = 10;
+
+            // Initialize default clients count -@Gurleen
+            clientsCount = 1;
+
         }
         #endregion
 
-        #region Menu Displays -@Pratiksha
+        #region Menu Displays -@Pratiksha and -@Gurleen
         //function to display the main menu lines -@Pratiksha
         static void displayMainMenu()
         {
@@ -200,7 +215,7 @@
         }
         //function to display the role menu lines -@Pratiksha
         static void displayRoleMenu()
-        {          
+        {
             Console.WriteLine("1.Employee");
             Console.WriteLine("2.Client");
             Console.WriteLine("Please enter the appropriate option");
@@ -211,6 +226,22 @@
             Console.WriteLine("1.Display all products");
             Console.WriteLine("2.Display purchases");
             Console.WriteLine("3.Sign Out");
+            Console.WriteLine("Please enter the appropriate option (1-3)");
+        }
+
+        //function to display the employee menu lines -@Gurleen
+        static void displayEmployeeMenu()
+        {
+            Console.WriteLine("1. Create products");
+            Console.WriteLine("2. Modify one product");
+            Console.WriteLine("3. Display all the products sorted by ID");
+            Console.WriteLine("4. Create a client");
+            Console.WriteLine("5. Modify a client");
+            Console.WriteLine("6. Display all the clients sorted by ID");
+            Console.WriteLine("7. Sell");
+            Console.WriteLine("8. Display all the sales");
+            Console.WriteLine("9. Display total of sales");
+            Console.WriteLine("10. Sign Out: to return to the main menu");
             Console.WriteLine("Please enter the appropriate option (1-3)");
         }
         #endregion
@@ -255,6 +286,30 @@
         }
         #endregion
 
+        #region Employee Verification -@Gurleen
+        /** function to check the unique ID and password and return
+        the employee if found or return a employee with ID 0  -@Gurleen
+        **/
+        static Employee verifyEmployee()
+        {
+            Console.WriteLine("Enter Employee ID:");
+            //read an interger between 100000 and 999999 -@Gurleen
+            int employeeId = readInteger(100000, 999999);
+
+            //read a string which is not empty or null -@Gurleen
+            Console.WriteLine("Enter Password:");
+            string password = getPassword();
+
+            //Verify the employee record and match -@Gurleen
+            if (employeeId == defaultEmployeeId && password == defaultEmployeePassword)
+            {
+                return employee; // Return verified client -@Gurleen
+            }
+            // Return a Client with ID 0 if verify fails -@Gurleen
+            return new Employee { UniqueId = 0 };
+        }
+        #endregion
+
         #region Client Functionality -@Pratiksha
         //function to display all products -@Pratiksha
         static void displayAllProducts()
@@ -278,13 +333,13 @@
         static void handleClientFunctionality(Client client)
         {
             bool exit = false;
-            
+
             while (!exit)
             {
-                
+
                 //function to client menu -@Pratiksha
                 displayClientMenu();
-                
+
                 //read the choice between 1 and 3 from the menu -@Pratiksha
                 int getChoice = readInteger(1, 3);
 
@@ -308,6 +363,184 @@
                 // return to main menu after signing out -@Pratikha
                 startProgram();
             }
+        }
+        #endregion 
+
+        #region Employee Functionality -@Gurleen
+        //function to create products -@Gurleen
+        static void createProducts()
+        {
+            string createProducts = "YES";
+            while (createProducts == "YES")
+            {
+                if (productsCount == maxProducts)
+                {
+                    Console.WriteLine("Unable to create more products, maximum products reached!");
+                    break;
+                }
+
+                Console.WriteLine("Enter Product ID:");
+                int productId = readInteger(100000, 999999);
+                Console.WriteLine("Enter Product Name:");
+                string productName = readString();
+                Console.WriteLine("Enter Subtotal:");
+                double unitPrice = readDouble();
+                int quantityAvailable = readInteger();
+                Console.WriteLine("Enter Quantity Available:");
+                product[productsCount] = new Product(productId, productName, unitPrice, quantityAvailable);
+                productsCount += 1;
+
+                Console.WriteLine("Do you want to create more products (Type Yes):");
+                createProducts = readString().ToUpper();
+            }
+
+        }
+
+        //function to modify one product -@Gurleen
+        static void modifyOneProduct()
+        {
+            Console.WriteLine("Enter Product ID:");
+            int productId = readInteger(100000, 999999);
+            //for loop to go through the products and match -@Gurleen
+            for (int i = 0; i < product.Length; i++)
+            {
+                if (product[i].UniqueId == productId)
+                {
+                    Console.WriteLine("Enter Product Name:");
+                    product[i].Name = readString();
+                    Console.WriteLine("Enter Subtotal:");
+                    product[i].UnitPrice = readDouble();
+                    product[i].QuantityAvailable = readInteger();
+                    Console.WriteLine("Enter Quantity Available:");
+                    break;
+                }
+            }
+            Console.WriteLine("Product not found!");
+        }
+
+        //function to display all products sorted by ID -@Gurleen
+        static void displayAllProductsSortedByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        //function to create a client -@Gurleen
+        static void createAClient()
+        {
+            if (clientsCount == maxClients)
+            {
+                Console.WriteLine("Unable to create more clients, maximum clients reached!");
+                break;
+            }
+            Console.WriteLine("Enter Client ID:");
+            int clientId = readInteger(100000, 999999);
+            Console.WriteLine("Enter First Name:");
+            string firstName = readString();
+            Console.WriteLine("Enter Last Name:");
+            string lastName = readString();
+            Console.WriteLine("Enter Password:");
+            string password = readString();
+            client[clientsCount] = new Client(clientId, firstName, lastName, password);
+            clientsCount += 1;
+        }
+
+        //function to modify a client -@Gurleen
+        static void modifyAClient()
+        {
+            Console.WriteLine("Enter Client ID:");
+            int clientId = readInteger(100000, 999999);
+            //for loop to go through the products and match -@Gurleen
+            for (int i = 0; i < client.Length; i++)
+            {
+                if (client[i].UniqueId == clientId)
+                {
+                    Console.WriteLine("Enter First Name:");
+                    client[i].Name = readString();
+                    Console.WriteLine("Enter Last Name:");
+                    client[i].lastName = readString();
+                    Console.WriteLine("Enter Password:");
+                    client[i].password = readString();
+                    break;
+                }
+            }
+            Console.WriteLine("Client not found!");
+        }
+
+        //function to display all clients sorted by ID -@Gurleen
+        static void displayAllClientsSortedByID()
+        {
+            throw new NotImplementedException();
+        }
+
+        //function to sell -@Gurleen
+        static void sell()
+        {
+            throw new NotImplementedException();
+        }
+
+        //function to display all sales -@Gurleen
+        static void displayAllSales()
+        {
+            throw new NotImplementedException();
+        }
+
+        //function to display total sales -@Gurleen
+        static void displayTotalSales()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //function to display Employee Menu and perform employee functions -@Gurleen
+        static void handleEmployeeFunctionality(Employee employee)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+
+                //function to client menu -@Gurleen
+                displayEmployeeMenu();
+
+                //read the choice between 1 and 3 from the menu -@Gurleen
+                int getChoice = readInteger(1, 3);
+
+                //switch statement for handling client functions -@Gurleen
+                switch (getChoice)
+                {
+                    case 1:
+                        createProducts();
+                        break;
+                    case 2:
+                        modifyOneProduct();
+                        break;
+                    case 3:
+                        displayAllProductsSortedByID();
+                        break;
+                    case 4:
+                        createAClient();
+                        break;
+                    case 5:
+                        modifyAClient();
+                        break;
+                    case 6:
+                        displayAllClientsSortedByID();
+                        break;
+                    case 7:
+                        sell();
+                        break;
+                    case 8:
+                        displayAllSales();
+                        break;
+                    case 9:
+                        displayTotalSales();
+                        break;
+                    case 10:
+                        exit = true;
+                        Console.WriteLine("Signing out....");
+                        break;
+                }
+            }
+            startProgram();
         }
         #endregion 
 
@@ -340,7 +573,18 @@
                 int roleChoice = readInteger(1, 2);
                 if (roleChoice == 1)
                 {
-                    // display employee and functions -@Pratiksha
+                    // display employee and functions -@Gurleen
+                    Employee employee = verifyEmployee();
+                    if (employee.UniqueId != 0)
+                    {
+                        //handle the employee functionalities with menu -@Gurleen
+                        handleEmployeeFunctionality(employee);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to authenticate user");
+                        startProgram();
+                    }
                 }
                 else if (roleChoice == 2)
                 {
@@ -372,9 +616,9 @@
         static void Main(string[] args)
         {
             initializeDefaultData();
+
             startProgram();
         }
     }
-
 
 }
